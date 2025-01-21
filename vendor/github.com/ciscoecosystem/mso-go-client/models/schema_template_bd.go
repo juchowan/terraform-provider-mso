0136d409eba08767adb496222fbf7356616e717c
Justyna Chowaniec <juchowan@cisco.com>
2025-01-21 10:49:07 +0100

    add ep move and fex pc

--- vendor/github.com/ciscoecosystem/mso-go-client/models/schema_template_bd.go
+++ vendor/github.com/ciscoecosystem/mso-go-client/models/schema_template_bd.go
@@ -6,8 +6,7 @@ type TemplateBD struct {
 	Value map[string]interface{} `json:",omitempty"`
 }
 
-func NewTemplateBD(ops, path, name, displayName, layer2Unicast, unkMcastAct, multiDstPktAct, v6unkMcastAct, vmac, description string, intersiteBumTrafficAllow, optimizeWanBandwidth, l2Stretch, l3MCast, arpFlood, unicastRouting bool, vrfRef, dhcpLabel map[string]interface{}, dhcpLabels []interface{}) *PatchPayload {
-	var bdMap map[string]interface{}
+func NewTemplateBD(ops, path, name, displayName, layer2Unicast, unkMcastAct, multiDstPktAct, v6unkMcastAct, vmac, epMoveDetectMode, description string, intersiteBumTrafficAllow, optimizeWanBandwidth, l2Stretch, l3MCast, arpFlood, unicastRouting bool, vrfRef, dhcpLabel map[string]interface{}, dhcpLabels []interface{}) *PatchPayload {	var bdMap map[string]interface{}
 	bdMap = map[string]interface{}{
 		"name":                     name,
 		"displayName":              displayName,
@@ -55,6 +54,10 @@ func NewTemplateBD(ops, path, name, displayName, layer2Unicast, unkMcastAct, mul
 
 	if bdMap["vmac"] == "" {
 		delete(bdMap, "vmac")
+	}
+
+	if epMoveDetectMode != "" {
+		bdMap["epMoveDetectMode"] = epMoveDetectMode
 	}
 
 	if len(dhcpLabel) == 0 {
